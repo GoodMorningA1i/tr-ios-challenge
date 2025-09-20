76e1621663daef55d5e1e444691ce4d637ddec86
Ali Syed <alisyed0206@gmail.com>
2025-09-20 15:48:33 -0400

    add starter files

---
+++ MovieBrowser/MovieBrowser/MovieBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBrowserApp.swift
+//  MovieBrowser
+//
+//  Created by Ali Syed on 2025-09-20.
+//
+
+import SwiftUI
+
+@main
+struct MovieBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
